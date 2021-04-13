9eb435cda5db2aa21a11ecbf210f0254eeef1ea9
Curtis Pritchard <curtis.pritchard@stclaircconnect.ca>
2021-04-13 14:37:53 -0400

    added a more aesthetic look in DBFZ CharSelect

--- src/Dragonball Fighter Z/CharacterSelect.js
+++ src/Dragonball Fighter Z/CharacterSelect.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FlatList, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { FlatList, SafeAreaView, ScrollView, View, Text, Dimensions } from 'react-native';
 import MatIcon from 'react-native-vector-icons/MaterialIcons'
 import Icon from 'react-native-vector-icons/AntDesign'
 import { globalStyle } from '../../themes/defaultTheme';
 
 const characterList = require('../../assets/data/characters.json');
 
-export const CharSelect = ({navigation}) => {
+export const FlatCharSelect = ({navigation}) => {
     React.useLayoutEffect( () => {
         navigation.setOptions({
             headerRight: () => (
@@ -22,3 +22,35 @@ export const CharSelect = ({navigation}) => {
     </SafeAreaView>
     );
 }
+
+
+export const GridCharacterSelect = ({navigation}) => {
+    React.useLayoutEffect( () => {
+        navigation.setOptions({
+            headerRight: () => (
+                <MatIcon name="settings" size={18} style={{padding:15}} color="#fff" onPress={() => {alert("Hello World")}} />
+            ),  
+        })
+    });
+
+    var items = [];
+    for (const character of characterList) {
+        const screenWidth = Dimensions.get('screen').width;
+        const numberOfCells = 3;
+        const cellMargin = 3;
+        var itemWidth = screenWidth / numberOfCells - cellMargin * 2;
+    
+        items.push(
+            <View style={{backgroundColor: '#666', width:itemWidth, height: itemWidth, margin: cellMargin, position: 'relative'}}><Text style={{color: 'white', position: 'absolute', bottom: 0, right: 0, padding: 5, backgroundColor: "#333"}}>{character.name}</Text></View>
+        )
+    }
+    return (
+        <SafeAreaView style={globalStyle.mainContainer}>
+            <ScrollView>
+                <View style={{flexDirection:'row', flexWrap: 'wrap'}}>
+                    {items}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+        );
+} 
